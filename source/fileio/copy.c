e24674ba2f09ad83f066eda04a3c6b64e566a55c
huangqinpingV1 <huangqinping4@gmai.com>
2023-04-24 18:18:22 +0800

    copy命令实现

--- source/fileio/copy.c
+++ source/fileio/copy.c
@@ -16,6 +16,34 @@ int main(int argc,char** argv){
 	char buf[BUF_SIZE];
 	
 	if(argc !=3  || strcmp(argv[1],"--help")==0)
-		usageErr("%s old-file new-file\n",argv[0]);	
-	return 0;
+		usageErr("%s old-file new-file\n",argv[0]);
+	/*Open   input and output files*/
+	inputFd  = open(argv[1],O_RDONLY);
+	if(inputFd == -1)
+		errExit("opening file %s",argv[1]);
+
+	openFlags  = O_CREAT|O_WRONLY|O_TRUNC;
+	filePerms = S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP
+		|S_IROTH|S_IWOTH|S_IRWXU;
+
+	outPutFd =open(argv[2],openFlags,filePerms);
+	if(outPutFd  == -1)
+		errExit("opening file %s",argv[2]);
+
+	/*Transfer data until we encounter end of input or an error*/
+	while((numRead  = read(inputFd,buf,BUF_SIZE)) >0){
+		if(write(outPutFd,buf,numRead)  != numRead){
+			fatal("write() returned error or ");
+		}
+	}
+	if(numRead == -1)
+		errExit("read");
+
+	if(close(inputFd) == -1)
+		errExit("close input ");
+	if(close(outPutFd) == -1)
+		errExit("close outPut");
+
+
+	exit(EXIT_SUCCESS);
 }
